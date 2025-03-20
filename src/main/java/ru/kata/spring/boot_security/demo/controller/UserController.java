4c5a086d3424977c00ca19aa084bd5e2fe3fef66
veezday <vlad-zav07@mail.ru>
2025-03-20 21:52:53 +0700

    solution

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,22 +1,18 @@
 package ru.kata.spring.boot_security.demo.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.kata.spring.boot_security.demo.model.Role;
+import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.model.User;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping
-    public String getUser(@AuthenticationPrincipal User user, Model model) {
-        model.addAttribute("user", user);
-        model.addAttribute("ROLE_USER", Role.USER);
-        model.addAttribute("ROLE_ADMIN", Role.ADMIN);
-        return "index";
+    @GetMapping("/get")
+    public ResponseEntity<User> getUser(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(user);
     }
 }
